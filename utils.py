238e12638f8c52815e1bc1ae9342c97652d6bc76
Erattakulangara <erattakulangara@iibi-sgl-m001.iowa.uiowa.edu>
2021-05-28 14:46:37 -0500

    save result

--- utils.py
+++ utils.py
@@ -1,9 +1,9 @@
 from keras.preprocessing.image import ImageDataGenerator
-from Utils import config
 import numpy as np
 import os
 import skimage.io as io
 import skimage.transform as trans
+from config import *
 
 
 def trainGenerator(batch_size,train_path, image_folder, mask_folder, aug_dict, target_img_size, image_color="grayscale", seed=20):
@@ -37,9 +37,10 @@ def trainGenerator(batch_size,train_path, image_folder, mask_folder, aug_dict, t
 
 # Test generator
 
-def testGenerator(test_path, num_image=2, flag_multi_class=False, target_img_size=config.target_img_size, as_gray=True):
-    for i in range(num_image):
-        img = io.imread(os.path.join(test_path, "%d.png" % i), as_gray=as_gray)
+def testGenerator(test_path, num_image=2, flag_multi_class=False, target_img_size=target_img_size, as_gray=True):
+    files = os.listdir(test_path)
+    for filename in files:
+        img = io.imread(os.path.join(test_path, filename), as_gray=as_gray)
         img = img / 255
         img = trans.resize(img, target_img_size)
         img = np.reshape(img, img.shape + (1,)) if (not flag_multi_class) else img
@@ -59,3 +60,13 @@ def output_file_path(test_name,n_epochs,n_images,Day,Date,articulator):
     filename = test_name+"_"+n_epochs+"_"+n_images+"_"+Day+"_"+Date+".hdf5"
     model_path =os.path.join("Output/Models", articulator, filename)
     return model_path
+
+#label visualizer
+
+def labelVisualize(num_class,color_dict,img):
+    #print('working')
+    img = img[:,:,0] if len(img.shape) == 3 else img
+    img_out = np.zeros(img.shape + (3,))
+    for i in range(num_class):
+        img_out[img == i,:] = color_dict[i]
+    return img_out / 255
